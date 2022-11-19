02c001ff3321fff0882f18298a976fb3dfab72eb
vnavacar <vnavacar@myuax.com>
2022-11-19 22:10:27 +0100

    Proyecto empezado

---
+++ src/main/java/com/example/accesoadatos/AccesoADatosApplication.java
@@ -0,0 +1,13 @@
+package com.example.accesoadatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesoADatosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccesoADatosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/accesoadatos/AccesoADatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accesoadatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesoADatosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
