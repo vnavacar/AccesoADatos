63344967719bb6dae4600efdda60db817e33d70a
vnavacar <vnavacar@myuax.com>
2022-11-20 19:33:26 +0100

    imagen

---
+++ src/main/java/com/example/accesoadatos/Main.java
@@ -0,0 +1,16 @@
+package com.example.accesoadatos;
+
+import org.springframework.boot.SpringApplication;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    static final Scanner sc = new Scanner(System.in);
+
+    public static void main(){
+
+    }
+
+}
--- src/main/java/com/example/accesoadatos/Objeto.java
+++ src/main/java/com/example/accesoadatos/Objeto.java
@@ -1,18 +1,22 @@
 package com.example.accesoadatos;
 
+
+import java.awt.image.BufferedImage;
+
 public class Objeto {
     private String nombre;
     private String Descripcion;
     private int id;
-    private double valorDouble;
+
+    private BufferedImage imagen;
     private float valorFloat;
 
 
-    public Objeto(String nombre, String Descripcion, int id, double valorDouble, float valorFloat) {
+    public Objeto(String nombre, String Descripcion, int id, BufferedImage imagen, float valorFloat) {
         this.nombre = nombre;
         this.Descripcion = Descripcion;
         this.id = id;
-        this.valorDouble = valorDouble;
+        this.imagen = imagen;
         this.valorFloat = valorFloat;
     }
 
@@ -26,12 +30,33 @@ public class Objeto {
     public int getId() {
         return id;
     }
-    public double getValorDouble() {
-        return valorDouble;
+    public BufferedImage getImagen() {
+        return imagen;
     }
     public float getValorFloat() {
         return valorFloat;
     }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setImagen(BufferedImage imagen) {
+        this.imagen = imagen;
+    }
+    public void setValorFloat(float valorFloat) {
+        this.valorFloat = valorFloat;
+    }
+
+    @Override
+    public String toString() {
+        return "Objeto [Descripcion=" + Descripcion + ", id=" + id + ", nombre=" + nombre + ", valorFloat=" + valorFloat + "]";
+    }
+
 
 }
 
